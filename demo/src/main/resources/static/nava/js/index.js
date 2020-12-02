43a4d15bd01494a343b6515be1a3c5d9496da3a3
Oliver <olivergogogo@qq.com>
2020-12-02 22:45:58 +0800

    img

---
+++ demo/src/main/resources/static/nava/js/index.js
@@ -0,0 +1,10 @@
+function getQueryString(name) {
+    let reg = new RegExp("(^|&)" + name + "=([^&]*)(&|$)", "i");
+    let r = window.location.search.substr(1).match(reg);
+    if (r != null) {
+        return decodeURIComponent(r[2]);
+    };
+    return null;
+}
+let id = getQueryString("id");
+$(`#${id}`).addClass("active");
